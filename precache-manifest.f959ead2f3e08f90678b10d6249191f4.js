1863af2dadda75c12d747d0229645ae3002324d7
sarat9 <sarat.e99@gmail.com>
2022-01-12 00:27:52 +0530

    Updates

---
+++ precache-manifest.f959ead2f3e08f90678b10d6249191f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61931d8e32f5189893ae0e854bb1973a",
+    "url": "/rick-and-morty-react-fun/index.html"
+  },
+  {
+    "revision": "75bbda8c8a13eef3972f",
+    "url": "/rick-and-morty-react-fun/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "93eaeca32a8c2c30f841",
+    "url": "/rick-and-morty-react-fun/static/js/2.8bc62db9.chunk.js"
+  },
+  {
+    "revision": "77d7b88d0e85879ee31fdb7f7e89dbe6",
+    "url": "/rick-and-morty-react-fun/static/js/2.8bc62db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d18b1d7c1ffbedab090",
+    "url": "/rick-and-morty-react-fun/static/js/3.ec907e82.chunk.js"
+  },
+  {
+    "revision": "75bbda8c8a13eef3972f",
+    "url": "/rick-and-morty-react-fun/static/js/main.1c16cbc0.chunk.js"
+  },
+  {
+    "revision": "ff33330578920c1c0d42",
+    "url": "/rick-and-morty-react-fun/static/js/runtime-main.2b205c10.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4870fc84a10506261f1ae9d9e914034f.js"
+  "/rick-and-morty-react-fun/precache-manifest.f959ead2f3e08f90678b10d6249191f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rick-and-morty-react-fun/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
