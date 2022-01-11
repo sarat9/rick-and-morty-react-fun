a4ccbb2d46398a022734be3ba0442aeea264e958
sarat9 <sarat.e99@gmail.com>
2022-01-12 00:10:30 +0530

    Updates

---
+++ precache-manifest.4870fc84a10506261f1ae9d9e914034f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90e4c11472a034048810479755119b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75bbda8c8a13eef3972f",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "93eaeca32a8c2c30f841",
+    "url": "./static/js/2.8bc62db9.chunk.js"
+  },
+  {
+    "revision": "77d7b88d0e85879ee31fdb7f7e89dbe6",
+    "url": "./static/js/2.8bc62db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d18b1d7c1ffbedab090",
+    "url": "./static/js/3.ec907e82.chunk.js"
+  },
+  {
+    "revision": "75bbda8c8a13eef3972f",
+    "url": "./static/js/main.1c16cbc0.chunk.js"
+  },
+  {
+    "revision": "1bc25cc3dd5c89ac86fb",
+    "url": "./static/js/runtime-main.88147c0c.js"
+  }
+]);
